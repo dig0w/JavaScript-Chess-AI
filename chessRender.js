9522a51d2ea08ea8349cb4fc37b25e5a8c3355a2
dig0w <dig0w.yt@gmail.com>
2025-11-21 08:27:31 +0000

    Update chessRender.js

--- chessRender.js
+++ chessRender.js
@@ -47,7 +47,7 @@ export class ChessRender {
             for (let c = 0; c < this.engine.cols; c++) {
                 const sq = document.createElement('button');
                 sq.classList.add('square');
-                if (r == 3 && c == 3) sq.classList.add('cordR');
+                if (r == 4 && c == 4) sq.classList.add('cordR');
                 sq.dataset.r = r;
                 sq.dataset.c = c;
 
